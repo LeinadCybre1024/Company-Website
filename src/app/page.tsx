ce93e25caea09030ecde78bf801e5ec383446581
Daniel Mwangi Ndungu <leinadcybre@gmail.com>
2024-05-10 11:58:34 +0300

    commit uno

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Blog from "@/components/Blog";
+import Brands from "@/components/Brands";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Testimonials from "@/components/Testimonials";
+import Video from "@/components/Video";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Reliable , Responsive and Convinitne",
+  description: "This is Home for Startup Nextjs Template",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+    </>
+  );
+}
